1183d2f10a499d7d37d0f1beae430c1af52fefc1
dougassis <dougassis@icloud.com>
2023-09-25 22:23:36 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RandomPicker;
+
+public static class Program
+{
+    private static void Main()
+    {
+        List<string> nameList = new List<string>
+        {
+            "Douglas Santana",
+            "Amanda Torres",
+            "Bruno Tavares",
+            "Tiago Silva",
+            "Rui Seabra",
+            "Magali Laiginhas",
+            "César Crista",
+            "Paulo Gama",
+            "Sidney Miranda"
+        };
+
+        Random random = new Random();
+        int randomIndex = random.Next(minValue: 0, maxValue: nameList.Count);
+
+        string selectedName = nameList[index: randomIndex];
+
+        Console.WriteLine($"Sorteado da vez: {selectedName}");
+    }
+}
